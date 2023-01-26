bfbdb90858523d95fde2850a3b16b271cdb1922b
ikerising12138 <kaixuan.qu18@gmail.com>
2023-01-26 12:55:02 -0800

    commiit

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,30 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test
+  public void testReverseInPlace2(){
+    int[] input1 = {1,2,3};
+    ArrayExamples.reverseInPlace(input1);
+    assertEquals(input1[0], 3);
+    assertEquals(input1[1], 2);
+    assertEquals(input1[2], 1);
+  }
+
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReverse2(){
+    int[] input1 = {1,2,3};
+    int[]output = ArrayExamples.reversed(input1);
+    assertEquals(output[0], 3);
+    assertEquals(output[1], 2);
+    assertEquals(output[2], 1);
+  }
+
+
 }
